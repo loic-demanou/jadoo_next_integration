8fe65f73739951366264d7c769ce38902167e2ce
loic-africadigitalizer <loic.demanou@africadigitalizer.com>
2023-06-26 23:41:35 +0000

    some days

--- app/components/category/Category.tsx
+++ app/components/category/Category.tsx
@@ -1,31 +1,74 @@
 import React from 'react'
 import CategoryItem from './CategoryItem'
 import { BsHouse } from 'react-icons/bs'
+import Image from 'next/image'
+
+export interface CategoryProps {
+    icon: React.ReactNode;
+    title: string;
+    description: string;
+}
 
 const Category = () => {
 
     const categories = [
         {
             id: 1,
-            icon: BsHouse,
+            icon: (
+                <span className='relative'>
+                    <span className=''>
+                        <svg className='w-52 h-52 absolute -left-14 -top-16' viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
+                            <path fill="#FFF1DA" d="M17.2,-13.4C21.4,-8.4,23.5,-1.3,23.5,8.2C23.6,17.7,21.8,29.5,13.8,36.4C5.7,43.3,-8.5,45.2,-15.3,39.1C-22.1,33,-21.6,18.9,-24.2,6C-26.7,-7,-32.5,-18.7,-29,-23.6C-25.5,-28.4,-12.7,-26.4,-3.1,-23.9C6.5,-21.4,13,-18.5,17.2,-13.4Z" transform="translate(100 100)" />
+                        </svg>
+                    </span>
+                    <Image src="/image/satellite.png" height={60} width={60} alt='sat' className='relative z-10' />
+                </span>
+            ),
             title: "Lorem ipsum dolor",
             description: "Lorem ipsum dolor qshdu qsuhd hqskhd jqhjksd Lorem ipsum dolor qshdu qsuhd hqskhd jqhjksd",
         },
         {
             id: 2,
-            icon: BsHouse,
+            icon:  (
+                <span className='relative'>
+                    <span className=''>
+                        <svg className='w-52 h-52 absolute -left-14 -top-16' viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
+                            <path fill="#FFF1DA" d="M17.2,-13.4C21.4,-8.4,23.5,-1.3,23.5,8.2C23.6,17.7,21.8,29.5,13.8,36.4C5.7,43.3,-8.5,45.2,-15.3,39.1C-22.1,33,-21.6,18.9,-24.2,6C-26.7,-7,-32.5,-18.7,-29,-23.6C-25.5,-28.4,-12.7,-26.4,-3.1,-23.9C6.5,-21.4,13,-18.5,17.2,-13.4Z" transform="translate(100 100)" />
+                        </svg>
+                    </span>
+                    <Image src="/image/plane.png" height={70} width={70} alt='plane' className='relative z-10' />
+                </span>
+            ),
             title: "Lorem ipsum dolor",
             description: "Lorem ipsum dolor qshdu qsuhd hqskhd jqhjksd Lorem ipsum dolor qshdu qsuhd hqskhd jqhjksd",
         },
         {
             id: 3,
-            icon: BsHouse,
+            icon:  (
+                <span className='relative'>
+                    <span className=''>
+                        <svg className='w-52 h-52 absolute -left-14 -top-16' viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
+                            <path fill="#FFF1DA" d="M17.2,-13.4C21.4,-8.4,23.5,-1.3,23.5,8.2C23.6,17.7,21.8,29.5,13.8,36.4C5.7,43.3,-8.5,45.2,-15.3,39.1C-22.1,33,-21.6,18.9,-24.2,6C-26.7,-7,-32.5,-18.7,-29,-23.6C-25.5,-28.4,-12.7,-26.4,-3.1,-23.9C6.5,-21.4,13,-18.5,17.2,-13.4Z" transform="translate(100 100)" />
+                        </svg>
+                    </span>
+                    <Image src="/image/groupMicro.png" height={36} width={36} alt='Microphone' className='relative z-10' />
+                </span>
+            ),
             title: "Lorem ipsum dolor",
             description: "Lorem ipsum dolor qshdu qsuhd hqskhd jqhjksd Lorem ipsum dolor qshdu qsuhd hqskhd jqhjksd",
         },
         {
             id: 4,
-            icon: BsHouse,
+            icon:  (
+                <span className='relative'>
+                    <span className=''>
+                        <svg className='w-52 h-52 absolute -left-14 -top-16' viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
+                            <path fill="#FFF1DA" d="M17.2,-13.4C21.4,-8.4,23.5,-1.3,23.5,8.2C23.6,17.7,21.8,29.5,13.8,36.4C5.7,43.3,-8.5,45.2,-15.3,39.1C-22.1,33,-21.6,18.9,-24.2,6C-26.7,-7,-32.5,-18.7,-29,-23.6C-25.5,-28.4,-12.7,-26.4,-3.1,-23.9C6.5,-21.4,13,-18.5,17.2,-13.4Z" transform="translate(100 100)" />
+                        </svg>
+                    </span>
+                    <Image src="/image/setting.png" height={60} width={60} alt='srtting' className='relative z-10' />
+                </span>
+            ),
             title: "Lorem ipsum dolor",
             description: "Lorem ipsum dolor qshdu qsuhd hqskhd jqhjksd Lorem ipsum dolor qshdu qsuhd hqskhd jqhjksd",
         },
@@ -33,14 +76,17 @@ const Category = () => {
 
   return (
     <div>
-        <div className=' text-center my-36'>
-            <p className='uppercase font-semibold text-lg text-[#5E6282]'>Category</p>
-            <p className='capitalize font-bold text-5xl text-[#14183E]' style={{ fontFamily: 'Volkhov' }}>
+        <div className='w-[80%] mx-auto text-center flex flex-col gap-8 my-12 md:my-24 lg:my-24 xl:my-28'>
+            <p className='uppercase font-semibold text-base md:text-lg mt-7 text-[#5E6282]'>Category</p>
+            <p className='capitalize font-bold text-4xl md:text-5xl lg:text-5xl text-[#14183E]' style={{ fontFamily: 'Volkhov' }}>
                 We Offer Best Services
             </p>
+            <div className='hidden md:inline md:absolute md:right-20'>
+                <Image src="/image/group.png" alt="right" height={150} width={150} />
+            </div>
         </div>
         <div >
-            <div className='w-[80%] mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8'>
+            <div className='w-[80%] mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 mb-10'>
                 {categories.map((category) => (
                     <CategoryItem key={category.id}
                     icon={category.icon} 
--- app/components/category/CategoryItem.tsx
+++ app/components/category/CategoryItem.tsx
@@ -1,21 +1,24 @@
 import { BsFillArrowDownRightSquareFill } from 'react-icons/bs';
 import { IconType } from 'react-icons'
 
-
 interface CategoryProps {
-    icon: IconType;
+    icon: React.ReactNode;
     title: string;
     description: string;
 }
 
 const CategoryItem: React.FC<CategoryProps> = ({icon: Icon, title, description}) => {
     return ( 
-        <div className='flex flex-col gap-4 justify-center items-center'>
-            <span>
-                <Icon size={40} className="text-neutral-600" /> 
-            </span>
-            <p className=' font-semibold text-[#1E1D4C]'>{title}</p>
-            <p className=' text-sm text-[#1E1D4C]'>{description}</p>
+        <div>
+            <div className='flex flex-col gap-4 justify-center items-center rounded-2xl border border-1 p-10 hover:shadow-2xl duration-200
+            cursor-pointer'>
+                <span>
+                    {/* <Icon size={40} className="text-neutral-600" />  */}
+                    {Icon}
+                </span>
+                <p className=' font-semibold text-[#1E1D4C]'>{title}</p>
+                <p className=' text-sm text-[#1E1D4C] text-justify' >{description}</p>
+            </div>
         </div>
      );
 }
